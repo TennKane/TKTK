12a9a2029378bd5542eae0feb288e3d207202b95
turkey <1281314239@qq.com>
2025-07-05 03:40:34 +0800

    Feat: Add Video core features (Upload, Play)

---
+++ src/main/java/com/tktk/mapper/CommentMapper.java
@@ -0,0 +1,15 @@
+package com.tktk.mapper;
+
+import com.tktk.video.pojo.Comment;
+import com.tktk.video.pojo.Video;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @author TennKane
+ */
+@Mapper
+public interface CommentMapper extends com.baomidou.mybatisplus.core.mapper.BaseMapper<Comment>{
+
+
+}
