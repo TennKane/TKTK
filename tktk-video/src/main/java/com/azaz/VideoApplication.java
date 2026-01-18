c1b208a315f9b44ce68e4adb27a94626ba55f11f
turkey <1281314239@qq.com>
2026-01-18 10:26:50 +0800

    Refactor: Extract Video service to independent module

---
+++ tktk-video/src/main/java/com/azaz/VideoApplication.java
@@ -0,0 +1,27 @@
+package com.tktk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author TennKane
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableDiscoveryClient
+@EnableAsync
+@EnableFeignClients(basePackages = "com.tktk.clients")
+public class VideoApplication {
+    public static void main(String[]args){
+        SpringApplication.run(VideoApplication.class);
+    }
+}
+
+
