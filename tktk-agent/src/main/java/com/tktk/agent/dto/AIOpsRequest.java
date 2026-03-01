87ee08e7224eab5912606c9802931cf5ed9f681b
turkey <1281314239@qq.com>
2026-03-01 16:45:00 +0800

    Refactor: Rename packages to com.tktk.agent to match project convention

---
+++ tktk-agent/src/main/java/com/tktk/agent/dto/AIOpsRequest.java
@@ -0,0 +1,15 @@
+package com.tktk.agent.dto;
+
+import lombok.Data;
+
+/**
+ * AIOps 请求 DTO
+ */
+@Data
+public class AIOpsRequest {
+    
+    /**
+     * 用户请求描述
+     */
+    private String userRequest;
+}
